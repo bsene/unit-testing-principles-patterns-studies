53b4993ddc1f602459018c2ebbcd6d0aa7cd225f
Birame SENE <bsene@pm.me>
2022-05-22 10:27:19 +0200

    INIT project

---
+++ bin/unit_testing_principles_patterns_studies.dart
@@ -0,0 +1,5 @@
+import 'package:unit_testing_principles_patterns_studies/unit_testing_principles_patterns_studies.dart' as unit_testing_principles_patterns_studies;
+
+void main(List<String> arguments) {
+  print('Hello world: ${unit_testing_principles_patterns_studies.calculate()}!');
+}
---
+++ test/unit_testing_principles_patterns_studies_test.dart
@@ -0,0 +1,8 @@
+import 'package:unit_testing_principles_patterns_studies/unit_testing_principles_patterns_studies.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
