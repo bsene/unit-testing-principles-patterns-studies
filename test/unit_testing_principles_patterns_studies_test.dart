42cd41fa06dcdd58b3666e73f17e890165577e7e
Birame SENE <bsene@pm.me>
2022-05-22 12:17:59 +0200

    remove scafolded files

--- test/unit_testing_principles_patterns_studies_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:unit_testing_principles_patterns_studies/unit_testing_principles_patterns_studies.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
